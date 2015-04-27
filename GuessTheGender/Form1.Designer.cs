56f087c67f6ba9b720048cd8ab5e0b080e5e60c0
ichen0129 <ichen.lo@gmail.com>
2015-04-27 23:36:59 +0800

    add gender hw

---
+++ GuessTheGender/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GuessTheGender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.namelabel = new System.Windows.Forms.Label();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // namelabel
+            // 
+            this.namelabel.AutoSize = true;
+            this.namelabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namelabel.Location = new System.Drawing.Point(21, 38);
+            this.namelabel.Name = "namelabel";
+            this.namelabel.Size = new System.Drawing.Size(126, 15);
+            this.namelabel.TabIndex = 0;
+            this.namelabel.Text = "Enter your name: ";
+            // 
+            // guessButton
+            // 
+            this.guessButton.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessButton.Location = new System.Drawing.Point(81, 99);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(123, 45);
+            this.guessButton.TabIndex = 1;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(143, 34);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(129, 22);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 193);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.namelabel);
+            this.Name = "Form1";
+            this.Text = "Guess the gender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label namelabel;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+    }
+}
+
---
+++ GuessTheGender/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessTheGender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            string name = nameTextBox.Text;
+            string gender = (randomNumber == 0) ? "Girl" : "Boy";
+
+            //MessageBox.Show(string.Format("Hello {0}!, you are a {1} ", name, gender));
+            DialogResult messageResult =
+                MessageBox.Show(string.Format("Hello {0}, you are {1}.", name, gender),
+                "Guess", MessageBoxButtons.YesNo);
+
+            if (messageResult == DialogResult.Yes)
+                MessageBox.Show("Guessed !");
+            else
+                MessageBox.Show("Wrong Guesses !");
+        }
+    }
+}
