9f28d293fca1ddb70b2c07ab6e0ff61582e8aed0
ichen0129 <ichen.lo@gmail.com>
2015-04-28 10:18:41 +0800

    加總時檢查輸入的值是否為合法的整數

--- Calcuate/Form1.cs
+++ Calcuate/Form1.cs
@@ -28,10 +28,38 @@ namespace Calcuate
 
         private void calcuateButton_Click(object sender, EventArgs e)
         {
-            int Number1 = int.Parse(numTextBox1.Text);
-            int Number2 = int.Parse(numTextBox2.Text);
-            int Number3 = int.Parse(numTextBox3.Text);
-            int Number4 = int.Parse(numTextBox4.Text);
+            int Number1, Number2, Number3, Number4;
+            if (!int.TryParse(numTextBox1.Text, out Number1))
+            {
+                MessageBox.Show("Number 1 is not a valid int");
+                return;
+            }
+            else
+                Number1 = int.Parse(numTextBox1.Text);
+
+            if (!int.TryParse(numTextBox2.Text, out Number2))
+            {
+                MessageBox.Show("Number 2 is not a valid int");
+                return;
+            }
+            else
+                Number2 = int.Parse(numTextBox2.Text);
+
+            if (!int.TryParse(numTextBox3.Text, out Number3))
+            {
+                MessageBox.Show("Number 3 is not a valid int");
+                return;
+            }
+            else
+                Number3 = int.Parse(numTextBox3.Text);
+
+            if (!int.TryParse(numTextBox4.Text, out Number4))
+            {
+                MessageBox.Show("Number 4 is not a valid int");
+                return;
+            }
+            else
+                Number4 = int.Parse(numTextBox4.Text);
 
             int Total = Number1 + Number2 + Number3 + Number4;
             MessageBox.Show("Total : " + Total.ToString());
